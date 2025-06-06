de473869485d5581ba99bad9f0a2ebfa5da1dce9
Nahid <nahid@glabbr.tech>
2025-06-06 10:53:27 +0530

    chnged url

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 export const isRunningOnLocal = true;
-// export const API_BASE = "https://ninjarin-be.onrender.com/api";
-export const API_BASE = "http://localhost:8000/api";
+export const API_BASE = "https://ninjarin-be.onrender.com/api";
+// export const API_BASE = "http://localhost:8000/api";
 export const API_VER = "v1";
